08d115eca63bc733c8099f6b5ecdb9d07256c365
Piyush Ajmani <piyushajmani5@gmail.com>
2016-12-27 16:17:12 +0530

    Made changes to model to support Android
    Signed-off-by: Piyush Ajmani <piyushajmani5@gmail.com>

---
+++ src/main/java/com/codingblocks/codequiz/models/Option.java
@@ -0,0 +1,9 @@
+package com.codingblocks.codequiz.models;
+
+/**
+ * Created by piyush0 on 27/12/16.
+ */
+public class Option {
+    String answer;
+    Boolean correct;
+}
--- src/main/java/com/codingblocks/codequiz/models/Question.java
+++ src/main/java/com/codingblocks/codequiz/models/Question.java
@@ -1,19 +1,16 @@
 package com.codingblocks.codequiz.models;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by championswimmer on 21/12/16.
  */
 public class Question {
 
+    Integer id;
     String question;
     ArrayList<Option> options;
     ArrayList<String> tags;
-
-    class Option {
-        String answer;
-        Boolean correct;
-    }
-
+    Date date_added;
 }
