74a8b708ddad2d7282021f7dd6ee8ae53974b45b
Piyush Ajmani <piyushajmani5@gmail.com>
2016-12-30 17:54:11 +0530

    More game stuff
    Signed-off-by: Piyush Ajmani <piyushajmani5@gmail.com>

--- src/main/java/com/codingblocks/codequiz/api/ChallengeHandler.java
+++ src/main/java/com/codingblocks/codequiz/api/ChallengeHandler.java
@@ -1,10 +1,8 @@
 package com.codingblocks.codequiz.api;
 
+import com.codingblocks.codequiz.dummy_utils.DummyChallenge;
 import com.codingblocks.codequiz.dummy_utils.DummyQuestions;
-import com.codingblocks.codequiz.models.Challenge;
-import com.codingblocks.codequiz.models.Question;
-import com.codingblocks.codequiz.models.Submission;
-import com.codingblocks.codequiz.models.User;
+import com.codingblocks.codequiz.models.*;
 import com.codingblocks.codequiz.utils.ReadQuery;
 import com.google.gson.*;
 import io.undertow.server.HttpServerExchange;
@@ -16,16 +14,21 @@ import java.util.ArrayList;
  * Created by piyush0 on 30/12/16.
  */
 public class ChallengeHandler extends RoutingHandler {
+
     public ChallengeHandler() {
         super();
         this.post("/create", this::POST_createChallenge);
         this.get("/{challengeId}", this::getChallengeQuestions);
         this.post("/submit/{challengeId}", this::POST_submitChallenge);
         this.get("/{challengeId}/result", this::GET_challengeResult);
+        this.get("/me", this::GET_myChallenges);
     }
 
     private void GET_myChallenges(HttpServerExchange exchange) throws Exception {
-        
+        ArrayList<ChallengeSmall> challengeSmall = DummyChallenge.getDummyChallengeSmalls();
+        Gson gson = new Gson();
+
+        exchange.getResponseSender().send(gson.toJson(challengeSmall));
     }
 
     private void GET_challengeResult(HttpServerExchange exchange) throws Exception {
@@ -39,7 +42,7 @@ public class ChallengeHandler extends RoutingHandler {
     private Challenge getChallenge(Integer id) {
 
         //TODO: Make this better.
-        return Challenge.getDummyChallenges().get(0);
+        return DummyChallenge.getDummyChallenges().get(0);
     }
 
     private void POST_submitChallenge(HttpServerExchange exchange) throws Exception {
@@ -82,27 +85,28 @@ public class ChallengeHandler extends RoutingHandler {
         JsonObject object = (JsonObject) new JsonParser().parse(ReadQuery.read(exchange));
 
         Integer numQues = gson.fromJson(object.get("numQues"), Integer.class);
-        ArrayList<User> users = getUsers(object.getAsJsonArray("users"), gson);
+        ArrayList<Integer> users = getUsers(object.getAsJsonArray("users"), gson);
         String topic = gson.fromJson(object.get("topic"), String.class);
 
         int cId = getChallengeId(users, numQues, topic);
+        System.out.println(cId);
 
         exchange.getResponseSender().send(gson.toJson(cId));
 
     }
 
-    private ArrayList<User> getUsers(JsonArray users, Gson gson) {
-        ArrayList<User> retVal = new ArrayList<>();
+    private ArrayList<Integer> getUsers(JsonArray users, Gson gson) {
+        ArrayList<Integer> retVal = new ArrayList<>();
 
         for (int i = 0; i < users.size(); i++) {
-            User u = gson.fromJson(users.get(i), User.class);
+            Integer u = gson.fromJson(users.get(i), Integer.class);
             retVal.add(u);
         }
 
         return retVal;
     }
 
-    private int getChallengeId(ArrayList<User> users, Integer numQues, String topic) {
+    private int getChallengeId(ArrayList<Integer> users, Integer numQues, String topic) {
         return 1; // TODO: Make this better.
     }
 
--- src/main/java/com/codingblocks/codequiz/models/Challenge.java
+++ src/main/java/com/codingblocks/codequiz/models/Challenge.java
@@ -22,7 +22,8 @@ public class Challenge {
 
 
     public Challenge(ArrayList<User> usersInGame,
-                     ArrayList<Double> score, ArrayList<Boolean> pendingList, Date date, String topic, Integer challenge_Id, ArrayList<Question> questions) {
+                     ArrayList<Double> score, ArrayList<Boolean> pendingList, Date date,
+                     String topic, Integer challenge_Id, ArrayList<Question> questions) {
         this.usersInGame = usersInGame;
         this.score = score;
         this.statuses = pendingList;
@@ -32,52 +33,5 @@ public class Challenge {
         this.questions = questions;
     }
 
-
-    public static ArrayList<Challenge> getDummyChallenges() {
-
-        ArrayList<Challenge> challenges = new ArrayList<>();
-
-        ArrayList<Boolean> statuses = new ArrayList<>();
-        for (int i = 0; i < DummyUser.getUsers().size(); i++) {
-            if (i % 2 == 0) {
-                statuses.add(true);
-            } else {
-                statuses.add(false);
-            }
-        }
-
-        ArrayList<Double> scores = new ArrayList<>();
-
-        scores.add(11.2);
-        scores.add(43.1);
-        scores.add(5.3);
-        scores.add(23.5);
-        scores.add(16.4);
-        scores.add(99.4);
-
-
-        Challenge challenge1 = new Challenge(DummyUser.getUsers(), scores, statuses, new Date(2016, 2, 3), "C++", 0, DummyQuestions.getDummyQuestions());
-
-
-        User user1 = new User("A", 23, "fns");
-        User user2 = new User("B", 24, "fnsfdsf");
-
-        ArrayList<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-
-
-        challenges.add(challenge1);
-
-        challenges.add(challenge1);
-
-        challenges.add(challenge1);
-
-        challenges.add(challenge1);
-
-        challenges.add(challenge1);
-
-        return challenges;
-    }
 
 }
